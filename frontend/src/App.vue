0e50d3e2d10629de14e68ff7995916538eec0ea0
careyarmst <careyarmst@earthlink.net>
2024-12-02 21:24:35 -0500

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+ <v-app>
+  <v-toolbar app>
+  </v-toolbar>
+  <v-content>
+    <br>
+    <NewMessage/>  
+    <br>
+    <messagesMWord/>
+  </v-content>
+    
+ </v-app>
+</template>
+
+<script>
+import messagesMWord from './components/messagesMword';
+import NewMessage from './components/NewMessage';
+
+export default {
+  name: 'App',
+
+  components: {
+    messagesMWord, NewMessage
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/components/messagesMword.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-flex sm8 offset-sm2>
+      <v-card>
+       <v-toolbar dark>
+         <v-toolbar-title>Messages</v-toolbar-title>
+         </v-toolbar>
+       <v-list>
+          <v-list-item v-for="(message, index) in store.state.messages" :key="index">
+             <v-list-item-content>
+                <v-list-item-title>{{ message }}</v-list-item-title>
+             </v-list-item-content>
+          </v-list-item>
+       </v-list>
+      </v-card>
+   </v-flex>
+ </template>
+  <script>
+ import axios from 'axios'
+ import store from '../store.js'
+    export default {
+       data() {
+          return {
+             store
+          };
+       },
+       async created() {
+         
+         store.state.messages = (await axios.get('http://localhost:3000/messages')).data
+
+                }
+    };
+ </script>
